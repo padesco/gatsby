44988450cdefb70173d410d19b2a039a33f8952a
padesco <pa.desco@live.com>
2022-11-20 17:46:32 +0100

    last commit before new branch

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,11 +4,10 @@ import { StaticImage } from 'gatsby-plugin-image'
 
 const pageStyles = {
   color: "#232129",
-  padding: 96,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
 }
 const listStyles = {
-  marginBottom: 96,
+  marginBottom: 20,
   paddingLeft: 0,
 }
 const listItemStyles = {
@@ -109,14 +108,15 @@ const links = [
 
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <Layout pageTitle="Home Page">
+    <div>
+    <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
         src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
-      />
-      </Layout>
+    />
+    </Layout>
+    <footer style={pageStyles}>
       <ul style={listStyles}>
         <li style={docLinkStyle}>
           <a
@@ -149,7 +149,8 @@ const IndexPage = () => {
         alt="Gatsby G Logo"
         src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
       />
-    </main>
+    </footer>
+    </div>
   )
 }
 
